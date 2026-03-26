b7c258e2db20606f5fd5bdc1996f021424c3e62a
Tiago de Paula <tiagodepalves@gmail.com>
2026-03-26 03:00:05 -0300

    fix(Alpaca): dependencies mismatch

---
+++ .scripts/test-alpaca.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+exec podman run --rm -it \
+  -v "${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY}":/run/w0 \
+  -e XDG_RUNTIME_DIR=/run \
+  -e WAYLAND_DISPLAY=w0 \
+  -e XDG_SESSION_TYPE=wayland \
+  -e GDK_BACKEND=wayland \
+  -v ./cache:/var/cache/pacman/pkg \
+  -v /etc/machine-id:/etc/machine-id:ro \
+  -v ./pacman.conf:/etc/pacman.conf:ro \
+  -v /var/lib/repo/aur:/var/lib/repo/aur:O \
+  --network=host \
+  docker.io/archlinux/archlinux:latest \
+  bash -euc 'pacman -Syu --noconfirm alpaca-ai; dbus-run-session -- alpaca'
